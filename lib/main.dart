73a2877fb980df2f3f9e56f3cdacef04073fea34
L Fernando De Pombo <2381303+depombo@users.noreply.github.com>
2024-08-10 14:18:25 -0700

    use auth gate and add google login

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'dart:async';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, TargetPlatform;
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:nimbus/gemini.dart';
 import 'package:nimbus/open_ai.dart';
@@ -27,9 +28,9 @@ void main() async {
     cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
   );
   FirebaseUIAuth.configureProviders([
-    if (defaultTargetPlatform == TargetPlatform.iOS ||
-        defaultTargetPlatform == TargetPlatform.macOS)
-      AppleProvider()
+    if (defaultTargetPlatform == TargetPlatform.macOS) AppleProvider(),
+    GoogleProvider(
+        clientId: DefaultFirebaseOptions.currentPlatform.iosClientId!)
   ]);
   runApp(Main());
 }
@@ -59,6 +60,20 @@ class _MainState extends State<Main> with WidgetsBindingObserver {
       ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: primary),
+        // used by Firebase SignInScreen
+        // https://github.com/firebase/FirebaseUI-Flutter/blob/main/docs/firebase-ui-auth/theming.md
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: TextButton.styleFrom(
+            textStyle: TextStyle(fontSize: 20.0),
+            splashFactory: NoSplash.splashFactory,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            backgroundColor: primary,
+            foregroundColor: Colors.white,
+          ),
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
       ),
@@ -79,7 +94,7 @@ class _AuthGate extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          return ContinueWithApple();
+          return _Login();
         }
         final user = snapshot.data;
         if (user != null) {
@@ -113,11 +128,78 @@ class _AuthGate extends StatelessWidget {
   }
 }
 
-class ContinueWithApple extends StatelessWidget {
+class _Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SignInScreen(
-      showAuthActionSwitch: false,
+      resizeToAvoidBottomInset: false,
+      providers: [
+        EmailAuthProvider(),
+      ],
+      sideBuilder: (context, constraints) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Image.network(
+            //   'logo.png',
+            //   height: 60,
+            //   width: 60,
+            // ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'Nimbus',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            )
+          ],
+        );
+      },
+      footerBuilder: (context, action) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              const Divider(
+                // color: grey300,
+                thickness: 0.7,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              AppleSignInButton(
+                  auth: FirebaseAuth.instance,
+                  loadingIndicator: Center(child: CircularProgressIndicator())),
+              const SizedBox(
+                height: 20,
+              ),
+              GoogleSignInButton(
+                  loadingIndicator: Center(child: CircularProgressIndicator()),
+                  auth: FirebaseAuth.instance,
+                  clientId: DefaultFirebaseOptions.currentPlatform.iosClientId!)
+            ],
+          ),
+        );
+      },
+      headerBuilder: (context, constraints, _) {
+        return Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              // Image.network(
+              //   'logo.png',
+              //   height: 50,
+              //   width: 50,
+              // ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
